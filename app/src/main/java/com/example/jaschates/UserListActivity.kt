57297c3786c7ababb3e2e0defc4e7cae6a594ee4
daon1213 <diaeiyo1213@naver.com>
2022-11-10 09:37:56 +0900

    🔧 try - catch 문 변경으로 인해 오류 발생 시 Log 기능 및 사용자 id 크기가 0이 아닐때만 데이터를 받아오도록 변경

--- app/src/main/java/com/example/jaschates/UserListActivity.kt
+++ app/src/main/java/com/example/jaschates/UserListActivity.kt
@@ -1,24 +1,25 @@
 package com.example.jaschates
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jaschates.databinding.ActivityUserListBinding
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.ktx.Firebase
 
 class UserListActivity : AppCompatActivity() {
     lateinit var binding: ActivityUserListBinding
     var array: MutableList<UserDTO> = arrayListOf()
     var uids: MutableList<String> = arrayListOf()
     val myUid = FirebaseAuth.getInstance().uid
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_user_list)
@@ -30,12 +31,11 @@ class UserListActivity : AppCompatActivity() {
                     array.add(item.toObject(UserDTO::class.java)!!)
                     uids.add(item.id)
                 }
-
             }
             binding.peopleListRecyclerview.adapter?.notifyDataSetChanged()
         }
         binding.peopleListRecyclerview.layoutManager = LinearLayoutManager(this)
-        binding.peopleListRecyclerview.adapter = RecyclerViewAdapter(array)
+        binding.peopleListRecyclerview.adapter = RecyclerviewAdapter(array)
         watchingMyUidVideoRequest()
     }
 
@@ -64,20 +64,26 @@ class UserListActivity : AppCompatActivity() {
     }
 
     fun removeChannelStr() {
-        var map: MutableMap<String, Any> = mutableMapOf<String, Any>()
+        var map: MutableMap<String, Any> = mutableMapOf()
         map["channel"] = FieldValue.delete()
         FirebaseFirestore.getInstance().collection("users").document(myUid!!).update(map)
     }
 
     fun openVideoActivity(channelId: String) {
-        val i = Intent(UserListActivity(), VideoCallActivity::class.java)
+        val i = Intent(this, VideoCallActivity::class.java)
         i.putExtra("channelId", channelId)
-        startActivity(i)
+        try {
+            startActivity(i)
+        } catch (e: Exception) {
+            Log.e("TAG", "openVideoActivity: ${e.printStackTrace()} ", e.cause )
+        }
     }
 
     fun createVideoChatRoom(position: Int, channel: String) {
-        var map: MutableMap<String, Any> = mutableMapOf<String, Any>()
+        var map: MutableMap<String, Any> = mutableMapOf()
         map["channel"] = channel
-        FirebaseFirestore.getInstance().collection("users").document(uids[position]).update(map)
+        if (uids.size != 0){
+            FirebaseFirestore.getInstance().collection("users").document(uids[position]).update(map)
+        }
     }
 }
