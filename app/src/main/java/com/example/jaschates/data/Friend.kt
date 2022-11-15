5ba935007ab763db331a402f6cd02d8ef37113de
daon1213 <diaeiyo1213@naver.com>
2022-11-15 18:09:06 +0900

    🔧 프로필 데이터 클래스 이름은 프렌드로 변경

---
+++ app/src/main/java/com/example/jaschates/data/Friend.kt
@@ -0,0 +1,7 @@
+package com.example.jaschates.data
+
+data class Friend(
+    val email : String? = null,
+    val name : String? = null,
+    val profileImageUrl : String? = null,
+    val uid : String? = null)
