3d1ddb7a847fe26a2c70db60596a22aff38e8e20
daon1213 <diaeiyo1213@naver.com>
2022-11-16 15:23:53 +0900

    💄🔧 Bottom Navigation 구현

--- app/src/main/java/com/example/jaschates/view/MainActivity.kt
+++ app/src/main/java/com/example/jaschates/view/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.jaschates.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.jaschates.R
+import com.example.jaschates.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private lateinit var homeFragment: HomeFragment
+private lateinit var chatFragment: ChatFragment
+private lateinit var profileFragment: ProfileFragment
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(BottomNavItemSelectedListener)
+        homeFragment = HomeFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.fragments_frame, homeFragment).commit()
+
     }
+    private val BottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
+        when(it.itemId){
+            R.id.menu_home -> {
+                homeFragment = HomeFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, homeFragment).commit()
+            }
+            R.id.menu_chat -> {
+                chatFragment = ChatFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, chatFragment).commit()
+            }
+            R.id.menu_profile -> {
+                profileFragment = ProfileFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, profileFragment).commit()
+            }
+        }
+        true
+    }
+
+
 }
