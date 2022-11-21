9fba0fe83cfc901bb1150c3383ccdcf96ba6e0bb
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-11-21 09:31:52 +0900

    ✨ 랜덤채팅용 모델 생성

--- app/src/main/java/com/example/jaschates/data/ChatRoomModel.kt
+++ app/src/main/java/com/example/jaschates/data/ChatRoomModel.kt
@@ -1,8 +1,10 @@
 package com.example.jaschates.data
 
-class ChatModel (
-    val users: HashMap<String, Boolean> = HashMap(),
+data class ChatRoomModel(
+    val user: HashMap<String, Any> = HashMap(),
+    val title: String? = null,
+    val titleImage: String? = null,
     val comments : HashMap<String, Comment> = HashMap()
-){
+) {
     class Comment(val uid: String? = null, val message: String? = null, val time: String? = null)
 }
