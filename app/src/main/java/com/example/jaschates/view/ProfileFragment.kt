71333c66324f9d12706b5c2efd88d824595e16af
daon1213 <diaeiyo1213@naver.com>
2022-11-21 19:37:59 +0900

    💄🔧 Binding 사용 위한 layout 태그 추가 및 사용 안하는 Import 정리

--- app/src/main/java/com/example/jaschates/view/ProfileFragment.kt
+++ app/src/main/java/com/example/jaschates/view/ProfileFragment.kt
@@ -1,7 +1,5 @@
 package com.example.jaschates.view
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -9,19 +7,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import android.provider.MediaStore
-import android.text.Editable
 import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.jaschates.R
 import com.example.jaschates.data.Friend
 import com.example.jaschates.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
