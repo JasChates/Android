1182f8aad5bf8fd8e182f81d6cce188fc7fce053
daon1213 <diaeiyo1213@naver.com>
2022-12-20 10:00:16 +0900

    🔧💄 Circle Image 의존성 추가 + 이름 변경 안되던 오류 수정 + Home Binding 오류 수정

--- app/src/main/java/com/example/jaschates/view/HomeFragment.kt
+++ app/src/main/java/com/example/jaschates/view/HomeFragment.kt
@@ -1,19 +1,11 @@
 package com.example.jaschates.view
 
 import android.annotation.SuppressLint
-import android.app.Activity.RESULT_OK
-import android.app.Dialog
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
